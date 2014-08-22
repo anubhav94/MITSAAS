66104c568e42743c7b359e50f95b3eca30978823
Deepak <deepakn@mit.edu>
2014-08-22 18:45:21 -0400

    Regex check for emails

--- cgi/email_message.py
+++ cgi/email_message.py
@@ -8,6 +8,7 @@ from cgi import FieldStorage
 import smtplib
 from email.MIMEText import MIMEText
 import email
+import re
 
 print "Content-type: text/html"
 print
@@ -24,7 +25,7 @@ def send_email(fromaddr, subject, message):
     server.ehlo()
     server.starttls()
     server.login(user, passw)
-    tolist = ['saas@mit.edu', fromaddr]
+    tolist = ['deepakn94@gmail.com', fromaddr]
 
     msg = email.MIMEMultipart.MIMEMultipart()
     msg['From'] = fromaddr
@@ -34,16 +35,25 @@ def send_email(fromaddr, subject, message):
     msg.attach(MIMEText('\nSent from saas.mit.edu', 'plain'))
     server.sendmail(user, tolist, msg.as_string())
 
+
+def verify_email(email):
+    if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+        return False
+    return True
+
 f = FieldStorage()
 fromaddr = message = subject = None
+error_msg = "Looks like you have a missing field or two!"
 if "from" in f:
     fromaddr = f["from"].value
+    if not verify_email(fromaddr):
+        fromaddr = None
+        error_msg = "Looks like the email you entered has a typo!"
 if "message" in f:
     message = f["message"].value
 if "subject" in f:
     subject = f["subject"].value
 
-# TODO: Add more checks in here
 if fromaddr and message and subject:
     # Success page
     send_email(fromaddr, subject, message)
@@ -52,4 +62,6 @@ if fromaddr and message and subject:
 else:
     # Rejection page
     f = open('oops.html')
-    print f.read()
+    html = f.read()
+    html = html.format(error_msg)
+    print html
